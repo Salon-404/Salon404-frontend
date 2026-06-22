3513eb6cdaa11e6beeaf487021e767164b99b524
KevinGLopezRocha <kevinterceros01@gmail.com>
2026-06-22 01:17:12 -0300

    Dashboard y metricas filtrables agregadas

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,7 @@ import { InvitacionForm } from "./components/invitados/InvitacionForm";
 import ProveedoresPage from "./pages/proveedores/ProveedoresPage";
 import CronogramaEvento from "./pages/cronograma/CronogramaEvento";
 import CateringPage from "./pages/eventos/CateringPage";
+import DashboardPage from "./pages/dashboard/DashboardPage";
 
 function ReservaRedirect() {
   const { id } = useParams();
@@ -40,7 +41,6 @@ function ReservaEditarRedirect() {
   return <RedirectConBanner to={`/eventos/${id}/editar`} />;
 }
 
-// Módulo Pagos — Mariano Figueroa
 export default function App() {
   return (
     <BrowserRouter>
@@ -129,6 +129,16 @@ export default function App() {
           element={<InvitacionForm />}
         />
 
+        {/* Módulo Dashboard — Kevin Lopez */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute rolRequerido={ROLES.ADMIN}>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+
         {/* Módulo Proveedores */}
         <Route
           path="/proveedores"
@@ -146,6 +156,8 @@ export default function App() {
             </ProtectedRoute>
           }
         />
+        {/* Si meten cualquier URL*/}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
